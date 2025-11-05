8dca00d33882fc68f2049daa85b36db7898960a8
Ackenieo <m13435714563@163.com>
2025-11-05 22:05:39 +0800

    feat-Ackenieo-redis存储聊天信息

--- themis-app/src/main/java/com/achobeta/themis/config/AgentConfig.java
+++ themis-app/src/main/java/com/achobeta/themis/config/AgentConfig.java
@@ -1,10 +1,10 @@
 package com.achobeta.themis.config;
 
 import com.achobeta.themis.domain.user.service.IChatService;
+import com.achobeta.themis.infrastructure.user.repo.RedisChatMemoryStore;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
 import dev.langchain4j.service.AiServices;
-import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 public class AgentConfig {
     @Autowired
     private AgentConfigProperties agentConfigProperties;
+    @Autowired
+    private RedisChatMemoryStore redisChatMemoryStore;
 
     @Bean
     public IChatService chatService() {
@@ -23,12 +25,12 @@ public class AgentConfig {
                 .apiKey(agentConfigProperties.getApiKey())
                 .modelName(agentConfigProperties.getModel())
                 .build();
-        InMemoryChatMemoryStore memoryStore = new InMemoryChatMemoryStore();
+
         return AiServices.builder(IChatService.class)
                 .streamingChatModel(model)
                 .chatMemoryProvider(conversationId ->
                         MessageWindowChatMemory.builder()
-                                .chatMemoryStore(memoryStore)
+                                .chatMemoryStore(redisChatMemoryStore)
                                 .id(conversationId)
                                 .maxMessages(100)
                                 .build()
